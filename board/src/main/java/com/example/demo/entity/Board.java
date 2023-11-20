fe994340ebc0a9013998c4f4446dd8f6c534c02e
mearae <qownsbjh@gmail.com>
2023-11-20 16:25:42 +0900

    Entity 설정

--- board/src/main/java/com/example/demo/entity/Board.java
+++ board/src/main/java/com/example/demo/entity/Board.java
@@ -1,13 +1,39 @@
 package com.example.demo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.sql.Date;
+import java.sql.Timestamp;
 
 @Entity
 public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private int userId;
+
+    @Column(nullable = false)
+    private String userName;
+
+    @Column(nullable = false)
+    private Date birthDate;
+
+    @Column(nullable = false)
+    private String gender;
+
+    @Column(nullable = false)
+    private Timestamp joinDate;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String roles;
+
+    @Column(nullable = false)
+    private String platform;
 }
