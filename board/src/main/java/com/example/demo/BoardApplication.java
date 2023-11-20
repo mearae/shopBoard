f7645384c2bcd77ec0498574bdf685f59f1ec822
mearae <qownsbjh@gmail.com>
2023-11-20 14:33:57 +0900

    기본 설정

--- board/src/main/java/com/example/demo/BoardApplication.java
+++ board/src/main/java/com/example/demo/BoardApplication.java
@@ -10,8 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // mysqltutorial.org -> start here -> 2. install mysql
 //	-> http://dev.mysql.com/downloads/installer/ 클릭
 //	-> 아래 걸로 다운로드 -> 로그인 없이 다운로드
-
-
+// mysql workbench 실행 -> + 클릭 -> 이름, 비번 설정
+//	-> 왼쪽에 schema 선택 후 오클릭 create schema(이름 green)
 @SpringBootApplication
 public class BoardApplication {
 
